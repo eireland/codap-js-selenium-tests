5d9f69b06e40278cb2c16ee385e0d0cee1da5d3d
eireland <eireland@concord.org>
2019-09-06 15:10:03 -0700

    Added date-time test file and script to load it for easy checking

--- test/specs/testOpenLocalFile.js
+++ test/specs/testOpenLocalFile.js
@@ -6,17 +6,23 @@ const table = new TableTileObject;
 
 const CODAP_URL = "https://codap.concord.org/releases/staging/";
 const homeDir = process.env['HOME']
-const filePath=homeDir+'/development/table-sharing-plugin-js-selenium-test/fixtures/TypesErrorSampleDoc.codap'
-
+const errorFilePath=homeDir+'/development/codap-js-selenium-tests/fixtures/TypesErrorSampleDoc.codap'
+const dateTimeFilePath=homeDir+'/development/codap-js-selenium-tests/fixtures/date-time-test.codap'
 
 context('Open a local file', ()=>{
-    before(()=>{
+    beforeEach(()=>{
         browser.url(CODAP_URL);
         browser.pause(3000);
     })
-    it('will open a local file in CODAP', ()=>{
+    it('will open a TypesErrorSampleDoc file in CODAP', ()=>{
         cfm.openDocFromModal();
-        cfm.openLocalDoc(filePath); 
+        cfm.openLocalDoc(errorFilePath); 
+        browser.pause(5000);
+        // expect(table.getCaseTableTile()).be.visible;
+    })
+    it('will open a date-time test doc in CODAP', ()=>{
+        cfm.openDocFromModal();
+        cfm.openLocalDoc(dateTimeFilePath); 
         browser.pause(5000);
         // expect(table.getCaseTableTile()).be.visible;
     })
